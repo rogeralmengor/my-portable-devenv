a4a9828f49a28f27b18d9dd0260476126e038e9c
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2025-12-05 06:59:30 +0100

    feat(gitgraph.lua): add file.

---
+++ nvim/lua/plugins/gitgraph.lua
@@ -0,0 +1,23 @@
+return {
+  'isakbm/gitgraph.nvim',
+  dependencies = { 'sindrets/diffview.nvim' },
+  opts = {
+    symbols = {
+      merge_commit = 'M',
+      commit = '*',
+    },
+    format = {
+      timestamp = '%H:%M:%S %d-%m-%Y',
+      fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
+    },
+  },
+  keys = {
+    {
+      "<leader>gl",
+      function()
+        require('gitgraph').draw({}, { all = true, max_count = 5000 })
+      end,
+      desc = "GitGraph - Draw",
+    },
+  },
+}
