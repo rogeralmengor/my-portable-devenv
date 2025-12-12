44b0f594aad1009b4e0d4c46151c7e46d365be1e
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2025-12-12 07:45:00 +0100

    refactor(terminal.lua): opens automatically powershell in windows os.

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -2,11 +2,23 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   config = function()
+    -- 1. DEFINE SHELL BASED ON OS (Moved here for reliable loading)
+    local sysname = vim.loop.os_uname().sysname
+    local terminal_shell
+
+    if sysname == "Linux" or sysname == "Darwin" then
+      terminal_shell = "bash" -- Or "zsh" if you prefer
+    elseif sysname:match("Windows") then
+      terminal_shell = "powershell.exe" -- Explicitly set PowerShell
+    else
+      terminal_shell = "sh"
+    end
+
     local Terminal = require("toggleterm.terminal").Terminal
 
     require("toggleterm").setup {
       size = 15,
-      open_mapping = [[<C-t>]], -- main floating terminal
+      open_mapping = [[<C-t>]],
       hide_numbers = true,
       shade_terminals = true,
       shading_factor = 2,
@@ -15,7 +27,10 @@ return {
       persist_size = true,
       direction = "float",
       close_on_exit = true,
-      shell = vim.o.shell, -- taken from init.lua (bash on Linux, pwsh on Windows)
+
+      -- 2. USE THE NEWLY DEFINED VARIABLE
+      shell = terminal_shell, 
+
       float_opts = {
         border = "curved",
         width = math.floor(vim.o.columns * 0.8),
@@ -24,20 +39,17 @@ return {
       },
     }
 
-    -- Fallback mapping (if <C-t> is intercepted by host terminal)
+    -- ... (rest of your existing code remains unchanged) ...
     vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
 
-    -- Function to toggle any numbered terminal
     local function toggle_term(num)
       Terminal:new({ count = num }):toggle()
     end
 
-    -- Map <leader>1..9 to toggle numbered terminals
     for i = 1, 9 do
       vim.keymap.set("n", "<Leader>" .. i, function() toggle_term(i) end, { noremap = true, silent = true })
     end
 
-    -- Terminal mode navigation (Ctrl-h/j/k/l)
     vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-w>h]], { noremap = true, silent = true })
     vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-w>j]], { noremap = true, silent = true })
     vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-w>k]], { noremap = true, silent = true })
