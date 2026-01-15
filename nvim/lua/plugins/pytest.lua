24e2c9f293dac48f9f35a45b96743ed7da887f5e
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2026-01-15 12:03:30 +0100

    chore(pytest): delete configuration.

--- nvim/lua/plugins/pytest.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "richardhapb/pytest.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  opts = {},
-  config = function(_, opts)
-    -- Simply initialize pytest; Treesitter is handled in its own file
-    require('pytest').setup(opts)
-  end
-}
