1100ad2ce04e6ea9eb57e6e09bc41f88e853b3d2
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2026-02-10 16:13:48 +0100

    chore(debugging): add neuranine configuration.

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -1,35 +1,87 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"leoluz/nvim-dap-go",
-		"rcarriga/nvim-dap-ui",
-    "mfussenegger/nvim-dap-python",
-    "nvim-neotest/nvim-nio"
-	},
-	config = function()
-    require("dapui").setup()
-		require("dap-go").setup()
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+	  "nvim-neotest/nvim-nio",
+      "rcarriga/nvim-dap-ui",
+      "mfussenegger/nvim-dap-python",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      local dap_python = require("dap-python")
 
-		local dap, dapui = require("dap"), require("dapui")
+      require("dapui").setup({})
+      require("nvim-dap-virtual-text").setup({
+        commented = true, -- Show virtual text alongside comment
+      })
 
-    require("dap-python").setup("python")
+      dap_python.setup("python3")
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+      vim.fn.sign_define("DapBreakpoint", {
+        text = "",
+        texthl = "DiagnosticSignError",
+        linehl = "",
+        numhl = "",
+      })
 
-		vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-	end,
+      vim.fn.sign_define("DapBreakpointRejected", {
+        text = "", -- or "❌"
+        texthl = "DiagnosticSignError",
+        linehl = "",
+        numhl = "",
+      })
+
+      vim.fn.sign_define("DapStopped", {
+        text = "", -- or "→"
+        texthl = "DiagnosticSignWarn",
+        linehl = "Visual",
+        numhl = "DiagnosticSignWarn",
+      })
+
+      -- Automatically open/close DAP UI
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+
+      local opts = { noremap = true, silent = true }
+
+      -- Toggle breakpoint
+      vim.keymap.set("n", "<leader>db", function()
+        dap.toggle_breakpoint()
+      end, opts)
+
+      -- Continue / Start
+      vim.keymap.set("n", "<leader>dc", function()
+        dap.continue()
+      end, opts)
+
+      -- Step Over
+      vim.keymap.set("n", "<leader>do", function()
+        dap.step_over()
+      end, opts)
+
+      -- Step Into
+      vim.keymap.set("n", "<leader>di", function()
+        dap.step_into()
+      end, opts)
+
+      -- Step Out
+      vim.keymap.set("n", "<leader>dO", function()
+        dap.step_out()
+      end, opts)
+			
+      -- Keymap to terminate debugging
+	  vim.keymap.set("n", "<leader>dq", function()
+	      require("dap").terminate()
+      end, opts)
+
+      -- Toggle DAP UI
+      vim.keymap.set("n", "<leader>du", function()
+        dapui.toggle()
+      end, opts)
+    end,
+  },
 }
+
