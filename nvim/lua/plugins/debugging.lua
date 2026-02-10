404f0ba0e5542b8585b37550277eea3d18b2a63f
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2026-02-10 17:18:35 +0100

    chore(debugging): force hard the usage of python in dap.

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -13,44 +13,42 @@ return {
       local dap_python = require("dap-python")
 
       require("dapui").setup({})
-      require("nvim-dap-virtual-text").setup({
-        commented = true,
-      })
+      require("nvim-dap-virtual-text").setup({ commented = true })
 
-      -- 1. Setup the plugin with the correct path
-      dap_python.setup("/opt/venv/bin/python3")
-
-      -- 2. FORCE the adapter to use the correct path 
-      -- This bypasses the logic that is looking for '/usr/src/.venv/bin/python'
+      -- 1. HARD OVERRIDE: Define the adapter manually to kill the broken path
       dap.adapters.python = {
         type = 'executable',
         command = '/opt/venv/bin/python3',
         args = { '-m', 'debugpy.adapter' },
       }
 
-      -- Gutter Signs
+      -- 2. RESET and REBUILD configurations
+      -- This clears out the ghost '/usr/src/.venv/bin/python' path
+      dap.configurations.python = {
+        {
+          type = 'python',
+          request = 'launch',
+          name = "Launch file",
+          program = "${file}",
+          pythonPath = function()
+            return '/opt/venv/bin/python3'
+          end,
+        },
+      }
+
+      -- Gutter signs
       vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError" })
-      vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DiagnosticSignError" })
       vim.fn.sign_define("DapStopped", { text = "", texthl = "DiagnosticSignWarn", linehl = "Visual" })
 
-      -- Automatically open/close DAP UI
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
+      -- UI Automation
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+      dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
 
       -- Keymaps
       local opts = { noremap = true, silent = true }
       vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, opts)
       vim.keymap.set("n", "<leader>dc", dap.continue, opts)
-      vim.keymap.set("n", "<leader>do", dap.step_over, opts)
-      vim.keymap.set("n", "<leader>di", dap.step_into, opts)
-      vim.keymap.set("n", "<leader>dO", dap.step_out, opts)
       vim.keymap.set("n", "<leader>dq", dap.terminate, opts)
       vim.keymap.set("n", "<leader>du", dapui.toggle, opts)
     end,
