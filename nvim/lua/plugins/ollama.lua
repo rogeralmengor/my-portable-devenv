4418a082fd4f84f995be46902188ff281cd0cc6e
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2026-02-18 15:35:17 +0100

    chore: add ollama.

--- nvim/lua/plugins/ollama.lua
+++ nvim/lua/plugins/ollama.lua
@@ -1,35 +1,33 @@
 return {
-    'codethenpizza/lazy-llama',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        require('lazy-llama').setup({
-            url = "http://localhost:11434",
-            model = "codellama:7b-instruct",
-            stream = true,
-        })
-        
-        -- Custom command to ask questions about selected code
-        vim.api.nvim_create_user_command('ExplainWithQuestion', function()
-            local utils = require('lazy-llama.utils')
-            local selected_text = utils.get_visual_selection()
-            local file_extension = utils.get_current_buffer_file_extension()
-            
-            -- Prompt user for their question
-            vim.ui.input({ prompt = 'Ask about the code: ' }, function(question)
-                if question and question ~= '' then
-                    local custom_prompt = string.format(
-                        "This code is from a file with extension %s:\n\n%s\n\nQuestion: %s",
-                        file_extension,
-                        selected_text,
-                        question
-                    )
-                    require('lazy-llama').get_explanation(custom_prompt)
-                end
-            end)
-        end, { range = true })
-        
-        -- Optional: Add a keymap for easier access
-        vim.api.nvim_set_keymap('v', '<leader>eq', ':<C-u>ExplainWithQuestion<CR>', 
-            { noremap = true, silent = true, desc = "Llama: Ask question about selection" })
+    "David-Kunz/gen.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        model = "codellama:7b-instruct", -- Matches your installed model
+        display_mode = "float",          -- Use a floating window
+        show_model = true,
+        width = 45,                      -- Width of the "sidebar"
+        height = 30,                     -- Height of the "sidebar"
+        -- This part positions the window on the right side
+        window_config = { 
+            relative = 'editor', 
+            row = 1, 
+            col = vim.o.columns - 47,    -- Dynamically push to the right edge
+            style = 'minimal', 
+            border = 'rounded' 
+        },
+    },
+    config = function(_, opts)
+        require('gen').setup(opts)
+
+        -- Custom command to ask a question (replacing your ExplainWithQuestion)
+        vim.keymap.set({ 'n', 'v' }, '<leader>eq', function()
+            local prompt = vim.fn.input("Ask Llama: ")
+            if prompt ~= "" then
+                require('gen').exec({ prompt = prompt })
+            end
+        end, { desc = "Ollama: Ask a question" })
+
+        -- Default Menu (to see Refactor, Explain, etc.)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ol', ':Gen<CR>', { desc = "Ollama Menu" })
     end
 }
