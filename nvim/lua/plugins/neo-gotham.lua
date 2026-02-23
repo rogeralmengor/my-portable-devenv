93b958eb92c606bbcf2d9518d0c7208770bb0702
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2026-02-23 13:20:34 +0100

    chore(neo-gotham): add colorscheme.

---
+++ nvim/lua/plugins/neo-gotham.lua
@@ -0,0 +1,8 @@
+return  { 'https://gitlab.com/shmerl/neogotham.git',
+    lazy = false, -- to make sure it's loaded on startup
+    priority = 1000, -- to load before other plugins
+    config = function()
+      vim.cmd.colorscheme('neogotham')
+    end
+  }
+
