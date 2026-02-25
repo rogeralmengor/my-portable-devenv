6aa24f5180474598287f2554bd4f8f0bde43a655
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2026-02-25 13:01:36 +0100

    chore: modify lua lang server, delete ruff.

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ruff" },
+        ensure_installed = { "lua_language-server" },
         automatic_installation = false,
       })
     end,
