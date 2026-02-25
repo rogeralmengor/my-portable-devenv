42c723fa32d52f2d13e43335df796fe1e4013f60
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2026-02-25 13:17:22 +0100

    chore(lps-config): change to mason-org.

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,16 +1,16 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     config = function()
       require("mason").setup()
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls" },
-        automatic_installation = false,
+        automatic_enable = false,
       })
     end,
   },
