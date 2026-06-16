8ad333b0d3beeb227f69bf2f60c5fae9316fd17e
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2026-06-16 07:32:35 +0200

    chore: vim-test reconfigure <leader>opts*2

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -1,12 +1,15 @@
 return {
     "vim-test/vim-test",
     dependencies = {
-	    "preservim/vimux"
+        "preservim/vimux"
     },
-    vim.keymap.set('n', '<leader>t', ':TestNearest<CR>'),
-    vim.keymap.set('n', '<leader>T', ':TestFile<CR>'),
-    vim.keymap.set('n', '<leader>a', ':TestSuite<CR>'),
-    vim.keymap.set('n', '<leader>l', ':TestLast<CR>'),
-    vim.keymap.set('n', '<leader>g', ':TestVisit<CR>'),
-    vim.cmd("let test#strategy = 'vimux'")
+    config = function()
+        vim.keymap.set('n', '<leader>tn', ':TestNearest<CR>')
+        vim.keymap.set('n', '<leader>tF', ':TestFile<CR>')
+        vim.keymap.set('n', '<leader>tS', ':TestSuite<CR>')
+        vim.keymap.set('n', '<leader>tl', ':TestLast<CR>')
+        vim.keymap.set('n', '<leader>tv', ':TestVisit<CR>')
+
+        vim.g['test#strategy'] = 'vimux'
+    end
 }
