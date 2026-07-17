01c508153bc6fcf6acf910176f47ae30f9df75aa
Roger Almengor Gonzalez <roger.almengor@gaf.de>
2026-07-17 08:43:45 +0200

    chore: add ollama ph4-mini model

--- nvim/lua/plugins/ollama.lua
+++ nvim/lua/plugins/ollama.lua
@@ -24,7 +24,7 @@ return {
         __inherited_from = "openai",
         api_key_name = "", 
         endpoint = "http://127.0.0.1:11434/v1",
-        model = "qwen2.5-coder:1.5b", -- Main LLM for code generation and chat
+        model = "phi4-mini", -- Main LLM for code generation and chat
       },
     },
 
